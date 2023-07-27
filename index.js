94a9bee8e691400a9bfb12972da2d466f90d6279
FireStreaker2 <michaelccc2019@outlook.com>
2023-07-26 21:12:10 -0700

    seo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const api = "https://api.consumet.org/anime/gogoanime/watch";
+const infoAPI = "https://api.consumet.org/anime/gogoanime/info";
 
 app.use(express.static("static"));
 
@@ -14,14 +15,17 @@ app.get("/watch/:anime", async (req, res) => {
     const anime = req.params.anime;
     const quality = req.query.quality;
     const title = anime.replace(/-/g, " ");
-    const url = `${api}/${anime}`;
+    const name = anime.slice(0, anime.indexOf("-episode-"));
   
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(`${api}/${anime}`);
         const data = response.data;
 
-        let index;
+        const info = await axios.get(`${infoAPI}/${name}`);
+        const description = info.data.description;
+        const image = info.data.image;
 
+        let index;
         switch (quality) {
             case "360p":
                 index = 0;
@@ -51,7 +55,22 @@ app.get("/watch/:anime", async (req, res) => {
             <!DOCTYPE HTML>
             <html>
                 <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta name="description" content="QuickAnime - Watch ${title} online free, english subbed.\n${description}">
+                    <meta name="keywords" content="QuickAnime />
+                    <meta name=”copyright” content=”FireStreaker2”>
+                    <meta property="og:title" content="QuickAnime" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://quickanime.firestreaker2.gq" />
+                    <meta property="og:image" content=${image} />
+                    <meta property="og:description" content="QuickAnime - Watch ${title} online free, english subbed.\n${description}" />
+                    <meta name="theme-color" content="#000000">
+                    <meta name="twitter:card" content="summary_large_image">
+
                     <title>${title} | QuickAnime</title>
+                    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
                     <link rel="stylesheet" href="/index.css" />
                 </head>
         
@@ -65,7 +84,7 @@ app.get("/watch/:anime", async (req, res) => {
                 </body>
             </html>
         `);
-        
+
     } catch (error) {
         console.log(error);
         res.status(500).send("Error occurred while fetching data.");
